5d94aab2c8ae6cb949a7fa159e2820dabe91a83a
limerentfox <christi@limerentfox.com>
2017-02-06 16:03:12 -0500

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,21 @@
 export function manageFriends(state, action){
+  if (action.type === 'ADD_FRIEND') {
+    return {
+        ...state,
+        friends: [...state.friends, action.payload]
+      }
+    } else if (action.type === 'REMOVE_FRIEND') {
+        let indexToRemove = state.friends.findIndex( friend =>
+        friend.id === action.payload )
+
+        let newFriends = [
+        ...state.friends.slice(0, indexToRemove),
+        ...state.friends.slice(indexToRemove + 1)
+      ]
+
+      return {...state, friends: newFriends}
+
+  } else {
+    return state
+  }
 }
